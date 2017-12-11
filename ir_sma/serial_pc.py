891abd4d5c6e9fd33dabe74d42c922292f18c4a0
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-11 12:25:38 +0900

    喜び表情を送っているが、あまりうまく動かない

--- ir_sma/serial_pc.py
+++ ir_sma/serial_pc.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import serial
+import socket
 import sys
 import threading
 import binascii
@@ -7,51 +8,30 @@ import binascii
 # シリアル通信の設定(
 ser = serial.Serial("/dev/ttyACM0", 19200, timeout=1)
 
-#ほんとうかよー
-def recvThread():
-    serial_data = ser.read()
-    print("read",serial_data)
-    if serial_data == 'H':
-        #ref ir_sma.ino to check the high-low order
-        str_high = ser.read()
-        data_high=binascii.b2a_hex(str_high)
-        int_high=int(data_high,16)
-        print('int_high',int_high)
-        #print("high",data_high.encode('hex'))
-        str_low = ser.read()
-        data_low=binascii.b2a_hex(str_low)
-        int_low=int(data_low,16)
-        print('int_low',int_low)
-        #print("low",data_low.encode('hex'))
-        #high = ((data_high << 8) & 0xFF)
-        high = (int_high << 8)
-        #high = bin(bin(data_high >> 8) & 0xFF)
-        low = (int_low& 0xFF)
-        read_val = (high << 8|low)
-        print('read_val',read_val)
-        return read_val
-
-def react_formula_thre(ir_value, threshold=50):
-    #should be continuous function
-    if ir_value > threshold:
-        sma_val=60
-        print('=========================send!')
-    else:
-        sma_val=0
-        print('===========not send')
-    return sma_val
-
 def react_formula(ir_value):
     sma_val=ir_value*2/3
     return sma_val
 
 if __name__ == '__main__':
+    host = "192.168.146.128" #お使いのサーバーのホスト名を入れます
+    port = 50000 #クライアントと同じPORTをしてあげます
 
-    #recvT = threading.Thread(target = recvThread)
-    #recvT.start()
-    read_data=None
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversock.bind((host,port)) #IPとPORTを指定してバインドします
+    serversock.listen(10) #接続の待ち受けをします（キューの最大数を指定）
+
+    print 'Waiting for connections...'
+    clientsock, client_address = serversock.accept() #接続されればデータを格納
 
     while True:
+        rcvmsg = clientsock.recv(1024)
+        face = rcvmsg.split(",")
+        face_int = map(int,face[0:5])
+        print face_int
+        deg = react_formula(face_int[1])
+        ser.write(str(deg)+"\0")
+
         serial_data = ser.read()
         if serial_data == 'H':
             str_high = ser.read()
@@ -68,9 +48,10 @@ if __name__ == '__main__':
             if read_val!= None:
                 print('main:read_data', int_val, read_val)
                 #ここでちゃんと数値が垂れ流されてる？
-                deg = react_formula(int_val)
+                #deg = react_formula(int_val)
                 #ser.write(deg)
                 #ser.write(str(deg)+"\0")
                 #ここでちゃんと遅れてる？
                 #終了処理などちゃんとして？
 
+    clientsock.close()
