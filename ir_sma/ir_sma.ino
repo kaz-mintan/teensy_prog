466de42543cf809cfe1d24bb13dc68d482e044f6
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-11 09:54:17 +0900

    とりあえず送受信、Teensy側

--- ir_sma/ir_sma.ino
+++ ir_sma/ir_sma.ino
@@ -1,7 +1,7 @@
 //#include <Servo.h>
 //Global variables
 #define SMA_PIN   9     //Pin # for SMA PWM on P2
-#define LED_PIN   13    //Pin # for on-board Teensy LED
+#define IR_PIN A0
 #define TIME_ON   1.5   //In Seconds (Less than 2s)
 #define TIME_OFF  20    //In Seconds (At least 30s)
 
@@ -14,7 +14,7 @@ int deg = 0;    // サーボの角度
 void setup() {
   // Declarations for Sensor and Actuator Pins
   pinMode(SMA_PIN,OUTPUT);
-  pinMode(LED_PIN,OUTPUT); // On-Board Indicator LED
+  pinMode(IR_PIN,INPUT);
 
   //Intialize Serial Communication
   Serial.begin(19200);
@@ -47,22 +47,32 @@ int serialNumVal(){
   }
 }
 
+double analog2distance(int ans){
+  double volt = ans * 5.0 / 1023 ;
+  //double distance = ( 9.5974 / volt ) -3.0086 ;
+  double distance = ( 18.679 / volt ) -4.774 ;
+  return distance;
+}
+
 // メインループ
 void loop() {
-  ir_value = analogRead(LED_PIN);
+  int ir_value = analogRead(IR_PIN);
   //face_value = :TODO
+  double ir_distance = analog2distance(ir_value);
+  //Serial.println(ir_value);
   sendIntData(ir_value);
   //sendIntData(ir_value,face_value);
 
   //receive calced deg value
   deg = serialNumVal();
-  Serial.println(deg);
-
-  if(deg>=0||deg<=100){
+  //Serial.println(deg);
+  analogWrite(SMA_PIN,0);
+  if(deg>0||deg<=100){
     analogWrite(SMA_PIN,map(deg, 0, 100, 0, 255)); //75% duty cycle = 191, 50% = 127, etc.
-    digitalWrite(LED_PIN,HIGH);
-    delay(TIME_ON*1000); // SMA ON time (ms)
-    analogWrite(SMA_PIN,0);
-    digitalWrite(LED_PIN, LOW);
+    //digitalWrite(LED_PIN,HIGH);
+    //delay(TIME_ON*1000); // SMA ON time (ms)
+    //analogWrite(SMA_PIN,0);
+    //digitalWrite(LED_PIN, LOW);
   }
+  
 }
