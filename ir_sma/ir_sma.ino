51837672aa5b7b23fd80fdaa81f26328c4e2f5e7
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2017-12-19 06:18:05 +0900

    commit latest ver.

--- ir_sma/ir_sma.ino
+++ ir_sma/ir_sma.ino
@@ -20,7 +20,7 @@ void setup() {
   pinMode(IR_PIN,INPUT);
 
   //Intialize Serial Communication
-  Serial.begin(921600);
+  Serial.begin(9600);
 }
 
 // int型のデータを送信する関数
@@ -38,10 +38,15 @@ int serialNumVal(){
      // 文字数が3以上 or 末尾文字がある場合の処理
     if (i > 2 || input[i] == '\0') {
       val = atoi(input);    // 文字列を数値に変換
-      Serial.write(input); // 文字列を送信
-      Serial.write("\n");
+      Serial.println(input[i]);
+      Serial.println(val);
+      //Serial.write(input); // 文字列を送信
+      //Serial.write("\n");
       i = 0;      // カウンタの初期化
+      //Serial.println(val);
+      
       return val;
+      
     }
     else {
       i++;
@@ -50,46 +55,54 @@ int serialNumVal(){
   }
 }
 
-double analog2distance(int ans){
-  double volt = ans * 5.0 / 1023 ;
-  //double distance = ( 9.5974 / volt ) -3.0086 ;
-  double distance = ( 18.679 / volt ) -4.774 ;
-  return distance;
+int recv_data = 0;
+
+int read_val(){
+  if ( Serial.available() >= sizeof('H') + sizeof(int) ) {
+    Serial.println("in");
+      // ヘッダの確認
+      if ( Serial.read() == 'H' ) {
+        int low = Serial.read(); // 下位バイトの読み取り
+        Serial.println(low);
+        int high = Serial.read(); // 上位バイトの読み取り
+        Serial.println(high);
+        recv_data = makeWord(high,low); // 上位バイトと下位バイトを合体させてint型データを復元
+      }
+  }
+  return recv_data;
 }
+
+int wait_count=0;
+bool sma_on=0;
+long ans ;
 
 // メインループ
 void loop() {
-  ir_value = analogRead(IR_PIN);
-  //face_value = :TODO
-  //double ir_distance = analog2distance(ir_value);
-  //Serial.println(ir_value);
-  sendIntData(ir_value);
-  //sendIntData(ir_value,face_value);
+  ans = 0 ;
+  for (i=0 ; i < 100 ; i++) {
+    ans  = ans + analogRead(IR_PIN) ;   // 指定のアナログピン(0番端子)から読取ります
+  }
+  val = ans/100;
+  if(val>0&&val<1024){
+    sendIntData(val);
+  }
 
-  //receive calced deg value
-  //deg = serialNumVal();
-  //Serial.println(ir_value);
-  //if (ir_value > 50){
-    //deg = 40;
-  //}
-  //deg = serialNumVal();
-  if(deg>20&&deg<=100){
+  deg=Serial.read();
+  
+  delay(100);
+    
+  if(deg>=10){
+    
     analogWrite(SMA_PIN,map(deg, 0, 100, 0, 255)); //75% duty cycle = 191, 50% = 127, etc.
     digitalWrite(LED_PIN,HIGH);
+    
     delay(TIME_ON*1000); // SMA ON time (ms)
+    deg=0;
     analogWrite(SMA_PIN,0);
     digitalWrite(LED_PIN, LOW);
-    deg=0;
+    wait_count=0;
   }
-  deg = 0;
-  //Serial.println(deg);
-  //analogWrite(SMA_PIN,0);
-  //if(deg>0||deg<=100){
-    //analogWrite(SMA_PIN,map(deg, 0, 100, 0, 255)); //75% duty cycle = 191, 50% = 127, etc.
-    //digitalWrite(LED_PIN,HIGH);
-    //delay(TIME_ON*1000); // SMA ON time (ms)
-    //analogWrite(SMA_PIN,0);
-    //digitalWrite(LED_PIN, LOW);
-  //}
   
 }
+
+
