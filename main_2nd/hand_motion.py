f0f09e94b91049de3ef87a6ba47496c03cc298bf
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-07 08:18:59 +0900

    dummy hand motion was wrong

--- main_2nd/hand_motion.py
+++ main_2nd/hand_motion.py
@@ -6,10 +6,14 @@ def assumed_z(present_z, limit_max, limit_min):
     array_size = 1
     assumed_dz = np.random.uniform(low=-0.01,high=0.01,size=array_size)
     assumed_z = assumed_dz + present_z
-    if assumed_z >= limit_max:
+    if assumed_z > limit_max:
+        print('limit_max')
         assumed_z = assumed_z - assumed_dz
-    elif assumed_z <= limit_min:
-        assumed_z = assumed_z + assumed_dz
+        print('assumed_z',assumed_z)
+    elif assumed_z < limit_min:
+        print('limit_min')
+        assumed_z = assumed_z - assumed_dz
+        print('assumed_z',assumed_z)
 
     return assumed_z
 
@@ -17,14 +21,13 @@ def get_ir(present_z):
     limit_max=1
     limit_min=0
     z = assumed_z(present_z,limit_max,limit_min)
-    print('hand/z',z)
     return z
 
 if __name__ == '__main__':
     loop_val = 100
     z_depth = 0
-    zlimit_max = 4
-    zlimit_min = -2
+    zlimit_max = 1
+    zlimit_min = 0
     z=np.zeros(loop_val)
     for i in range(loop_val):
         z[i]=z_depth
