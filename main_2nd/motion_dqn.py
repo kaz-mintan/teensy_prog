2bd4bd2d9d87b0569dacd3e14b299bbad3c2b153
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-09 08:31:28 +0900

    increased value of weight

--- main_2nd/motion_dqn.py
+++ main_2nd/motion_dqn.py
@@ -8,6 +8,9 @@ from dummy_evaluator import *
 from neural_network import *
 from datetime import datetime
 from serial_pc import *
+from save_action_fig import *
+
+import matplotlib.pyplot as plt
 
 
 import sys
@@ -85,7 +88,7 @@ random = np.zeros(num_episodes)
 face_predict = np.zeros((1,type_face))
 q_predicted = np.zeros(num_episodes)
 
-state[:,0] = np.array([1,0,0,0,0,0.30])
+state[:,0] = np.array([1,0,0,0,0,0.50])
 action[:,0] = np.random.uniform(0,1)
 
 possible_a = np.linspace(0,1,100)
@@ -93,15 +96,15 @@ possible_a = np.linspace(0,1,100)
 ## set qfunction as nn
 q_input_size = type_face + type_ir + type_action
 q_output_size = 1
-q_hidden_size = (q_input_size + q_output_size )/3
+q_hidden_size = (q_input_size + q_output_size )/2
 
 q_teacher = np.zeros((q_output_size,num_episodes))
 
 Q_func = Neural(q_input_size, q_hidden_size, q_output_size, epsilon, mu, epoch)
-q_first_iteacher = np.random.uniform(low=0,high=0.001,size=(q_input_size,1))
-q_first_oteacher = np.random.uniform(low=0,high=0.001,size=(q_output_size,1))
+#q_first_iteacher = np.random.uniform(low=0,high=1,size=(q_input_size,1))
+#q_first_oteacher = np.random.uniform(low=0,high=0.001,size=(q_output_size,1))
 
-Q_func.train(q_first_iteacher.T,q_first_oteacher.T)
+#Q_func.train(q_first_iteacher.T,q_first_oteacher.T)
 
 if mode == 'predict':
     ## set predict function as nn
@@ -129,12 +132,14 @@ for episode in range(num_episodes-1):  #repeat for number of trials
     state_mean[:,episode+1]=seq2feature(state)
 
     ### calcurate r_{t}
-    reward[episode] = calc_reward(state, state_predict,
+    reward[episode+1] = calc_reward(state, state_predict,
             state_before,t_window, mode)
+    print('acted',action[:,episode],'reward',reward[episode+1])
 
     ### calcurate a_{t+1} based on s_{t+1}
+    random_rate = 0.4# * (1 / (episode + 1))
     random[episode+1], action[:,episode+1],next_q = Q_func.gen_action(possible_a,
-            num_action, num_face, state_mean, episode)
+            num_action, num_face, state_mean, episode,random_rate,action,reward,alpha)
 
     q_predicted[episode]=next_q
     q_teacher = Q_func.update(state_mean,num_action,num_face,action,episode,q_teacher,reward,next_q, select_episode, gamma, alpha)
@@ -145,12 +150,12 @@ for episode in range(num_episodes-1):  #repeat for number of trials
                 select_episode, gamma, alpha)
 
     before_state = state[:,t_window-1]
-    #acted = action[:,episode+1]
-    #rewed = reward[episode]
-    print('epi',episode,target_type,target_direct,mode,'ran',random[episode],'act',action[:,episode],'rwd',reward[episode])
+    #print('epi',episode,target_type,target_direct,mode,'ran',random[episode],'act',action[:,episode],'rwd',reward[episode+1])
 
     state_before = state
-    print('q_val',q_teacher[:,episode])
+    print('')
+
+save_file(num_episodes,action,target_type,target_direct,mode)
 
 np.savetxt('action_pwm.csv', action[0,:], fmt="%.3f", delimiter=",")
 np.savetxt('reward_seq.csv', reward, fmt="%.5f",delimiter=",")
