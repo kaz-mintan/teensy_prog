e65ef87b478620ddce5c1f81c430953b5c6df397
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-12 07:01:03 +0900

    class to set serial port and output read value

--- main_2nd/serial_com/serial_read.py
+++ main_2nd/serial_com/serial_read.py
@@ -5,30 +5,43 @@ import sys
 import threading
 import binascii
 
-# シリアル通信の設定(
-ser = serial.Serial("/dev/ttyACM0", 19200, timeout=1)
+#self.ser = serial.Serial("/dev/ttyACM0", 19200, timeout=1)
 
-def react_formula(ir_value):
-    sma_val=ir_value*2/3
-    return sma_val
+# シリアル通信の設定(
+class serial_read:
+    def __init__(self, port, baud):
+        self.ser = serial.Serial(port, baud, timeout=1)
+
+    def read_val(self):
+        while True:
+            serial_data = self.ser.read()
+            if serial_data == 'H':
+                str_high = self.ser.read()
+                str_low = self.ser.read()
+                read_val = conbine_high_low(str_high,str_low)
+                self.ser.flushInput()
+                if read_val!= None:
+                    return read_val
+
+def conbine_high_low(str_high,str_low):
+    data_high=binascii.b2a_hex(str_high)
+    int_high=int(data_high,16)
+    high = (int_high << 8)
+
+    data_low=binascii.b2a_hex(str_low)
+    int_low=int(data_low,16)
+    low = (int_low& 0xFF)
+
+    read_val = (high << 8|low)
+    int_val = int(read_val)
+    return int_val
 
 if __name__ == '__main__':
+    ser_port = "/dev/ttyACM0"
+    ser_baud = 19200
 
+    serial_test = serial_read(ser_port,ser_baud)
     while True:
-        serial_data = ser.read()
-        if serial_data == 'H':
-            str_high = ser.read()
-            data_high=binascii.b2a_hex(str_high)
-            int_high=int(data_high,16)
-            str_low = ser.read()
-            data_low=binascii.b2a_hex(str_low)
-            int_low=int(data_low,16)
-            high = (int_high << 8)
-            low = (int_low& 0xFF)
-            read_val = (high << 8|low)
-            int_val = int(read_val)
-            ser.flushInput()
-        #read_data = recvThread()
-            if read_val!= None:
-                print(read_val)
+        val = serial_test.read_val()
+        print(val)
 
