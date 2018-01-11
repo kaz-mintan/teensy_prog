05062e74b272394e53d8c3d842e9f463865095fd
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-11 10:03:12 +0900

    not-moved version

--- ir_sma/ir_sma.ino
+++ ir_sma/ir_sma.ino
@@ -15,9 +15,9 @@ int ir_value = 0;
 
 void setup() {
   // Declarations for Sensor and Actuator Pins
-  pinMode(SMA_PIN,OUTPUT);
-  pinMode(LED_PIN,OUTPUT); // On-Board Indicator LED
-  pinMode(IR_PIN,INPUT);
+  pinMode(SMA_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT); // On-Board Indicator LED
+  pinMode(IR_PIN, INPUT);
 
   //Intialize Serial Communication
   Serial.begin(9600);
@@ -31,22 +31,17 @@ void sendIntData(int value) {
 }
 
 // シリアル通信で受信したデータを数値に変換
-int serialNumVal(){
+int serialNumVal() {
   // データ受信した場合の処理
   if (Serial.available()) {
     input[i] = Serial.read();
-     // 文字数が3以上 or 末尾文字がある場合の処理
+    // 文字数が3以上 or 末尾文字がある場合の処理
     if (i > 2 || input[i] == '\0') {
       val = atoi(input);    // 文字列を数値に変換
       Serial.println(input[i]);
       Serial.println(val);
-      //Serial.write(input); // 文字列を送信
-      //Serial.write("\n");
       i = 0;      // カウンタの初期化
-      //Serial.println(val);
-      
       return val;
-      
     }
     else {
       i++;
@@ -57,52 +52,55 @@ int serialNumVal(){
 
 int recv_data = 0;
 
-int read_val(){
+int read_val() {
   if ( Serial.available() >= sizeof('H') + sizeof(int) ) {
     Serial.println("in");
-      // ヘッダの確認
-      if ( Serial.read() == 'H' ) {
-        int low = Serial.read(); // 下位バイトの読み取り
-        Serial.println(low);
-        int high = Serial.read(); // 上位バイトの読み取り
-        Serial.println(high);
-        recv_data = makeWord(high,low); // 上位バイトと下位バイトを合体させてint型データを復元
-      }
+    // ヘッダの確認
+    if ( Serial.read() == 'H' ) {
+      int low = Serial.read(); // 下位バイトの読み取り
+      Serial.println(low);
+      int high = Serial.read(); // 上位バイトの読み取り
+      Serial.println(high);
+      recv_data = makeWord(high, low); // 上位バイトと下位バイトを合体させてint型データを復元
+    }
   }
   return recv_data;
 }
 
-int wait_count=0;
-bool sma_on=0;
+int wait_count = 0;
+bool sma_on = 0;
 long ans ;
+bool read_flg = 1;
 
 // メインループ
 void loop() {
   ans = 0 ;
-  for (i=0 ; i < 100 ; i++) {
-    ans  = ans + analogRead(IR_PIN) ;   // 指定のアナログピン(0番端子)から読取ります
-  }
-  val = ans/100;
-  if(val>0&&val<1024){
-    sendIntData(val);
+  //for (i=0 ; i < 100 ; i++) {
+  //  ans  = ans + analogRead(IR_PIN) ;   // 指定のアナログピン(0番端子)から読取ります
+  //}
+  //val = ans/100;
+  //if(val>0&&val<1024){
+  //  sendIntData(val);
+  //}
+  if(read_flg == 1){
+    deg = Serial.read();
+    read_flg = 0;
   }
 
-  deg=Serial.read();
-  
   delay(100);
+
+  if (deg <= 60) {
+    analogWrite(SMA_PIN, map(deg, 0, 100, 0, 255)); //75% duty cycle = 191, 50% = 127, etc.
+    digitalWrite(LED_PIN, HIGH);
     
-  if(deg>=10){
-    
-    analogWrite(SMA_PIN,map(deg, 0, 100, 0, 255)); //75% duty cycle = 191, 50% = 127, etc.
-    digitalWrite(LED_PIN,HIGH);
-    
-    delay(TIME_ON*1000); // SMA ON time (ms)
-    deg=0;
-    analogWrite(SMA_PIN,0);
+    delay(TIME_ON * 1000); // SMA ON time (ms)
+    deg = 0;
+    analogWrite(SMA_PIN, 0);
     digitalWrite(LED_PIN, LOW);
-    wait_count=0;
+    wait_count = 0;
+    read_flg =1;
   }
-  
+
 }
 
 
