03f65586a58db4f7810509d6844d64878ec55519
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-12 21:12:21 +0900

    worked

--- main_2nd/serial_com/serial_double.py
+++ main_2nd/serial_com/serial_double.py
@@ -3,6 +3,8 @@
 from serial_read import *
 from serial_sma import *
 
+import threading
+
 if __name__ == '__main__':
     ser_port_sma = "/dev/ttyACM0"
     ser_port_ir = "/dev/ttyACM1"
@@ -10,7 +12,13 @@ if __name__ == '__main__':
 
     serial_ir = serial_read(ser_port_ir,ser_baud)
     serial_sma = serial_sma(ser_port_sma,ser_baud)
+
+    th_rcv = threading.Thread(target=serial_ir.read_val,name="th_rcv",args=())
+    th_rcv.start()
+
     while True:
-        val = serial_ir.read_val()
-        print(val)
+        deg = raw_input()
+        serial_sma.send_val(deg)
+        #val = serial_ir.read_val()
+        #print(val)
 
