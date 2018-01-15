3a42d56572a1e0552ef91b70013fe47ee7e1a74f
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-15 10:50:47 +0900

    changed to fit the motion_dqn.py

--- main_2nd/test_linear.py
+++ main_2nd/test_linear.py
@@ -4,14 +4,13 @@ import math
 
 def linear_state(single_state):
     for i in range(single_state.shape[1]):
-        print('single_state[:,i]',single_state[:,i])
-        print('shape',single_state[:,i].shape[0])
-        x = np.linspace(0,1,single_state[:,i].shape[0])
+        x = np.linspace(0,1,single_state[i,:].shape[0])
         #fitting
-        a, b = np.polyfit(x, single_state, 1)
+        a, b = np.polyfit(x, single_state[i,:], 1)
         #fitting line
     return a
 
+'''
 type_face = 5
 type_ir = 5
 t_window = 4
@@ -23,3 +22,4 @@ state = np.array([[1,1,2],[2,1,3],[5,5,1],[2,2,2]])
 print(state)
 fh = linear_state(state)
 print('fh',fh)
+'''
