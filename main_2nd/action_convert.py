0ae821ea9b1f8fe84aefae23ffafde98e436b514
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-16 08:40:25 +0900

    applied using numpy

--- main_2nd/action_convert.py
+++ main_2nd/action_convert.py
@@ -2,10 +2,10 @@
 import numpy as np
 
 def convert_action(action):
-    print(action)
     pwm_input = 50*action[0]+10
     keep = 1.5*action[1]+0.2
     delay = 3.0*action[2] + 0.2
+    print('action',pwm_input,keep,delay)
     return np.array([pwm_input,keep,delay])
 
 if __name__ == '__main__':
