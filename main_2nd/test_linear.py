423e10e4707d5c064fa91c688e18f197d082abf8
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-15 09:00:18 +0900

    add test_linear function

--- main_2nd/test_linear.py
+++ main_2nd/test_linear.py
@@ -2,8 +2,15 @@
 import numpy as np
 import math
 
+def linear_state(single_state):
+    x = np.linspace(0,1,single_state.shape[0])
+    #fitting
+    a, b = np.polyfit(x, single_state, 1)
+    #fitting line
+    return a
 
-def linear_state(state):
-    state_feature = np.mean(state, axis=1)
-    return state_feature
 
+state = np.array([1,1,2,3,1,2,3,3,6,6,4,2,1,2,3,1,2])
+print(state.shape[0])
+fh = linear_state(state)
+print(fh)
