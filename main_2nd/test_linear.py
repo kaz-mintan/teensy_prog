29d0b490022cc3be3a08540bcf2ca31648d57698
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-16 08:48:36 +0900

    add test_atan

--- main_2nd/test_linear.py
+++ main_2nd/test_linear.py
@@ -4,13 +4,20 @@ import math
 
 type_ir = 5
 
+def test_atan(a):
+    val = float(a)
+    return math.atan(val)*2/math.pi
+
 def linear_state(single_state):
+    print('line/single_state',single_state)
     for i in range(single_state.shape[1]):
         x = np.linspace(0,1,single_state[i,:].shape[0])
         #fitting
         a, b = np.polyfit(x, single_state[i,:], 1)
         #fitting line
-    return a
+
+    return math.atan(a)*2/math.pi
+    #return a
 
 def check_thre(ir_sensor,thre):
     ret = -1
@@ -22,4 +29,7 @@ def check_thre(ir_sensor,thre):
             ret = 0
     return ret
 
+if __name__ == '__main__':
+    a = raw_input()
+    print(test_atan(a))
 
