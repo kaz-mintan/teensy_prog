1f76a29a2e4974034d3b7838d1195f69f2dd81d4
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-16 13:14:52 +0900

    at this point, rm the type branch

--- main_2nd/reward_calc.py
+++ main_2nd/reward_calc.py
@@ -28,24 +28,6 @@ def reward_function(state, state_predict, state_before, mode):
     # extract face array (must be time sequence data)
     T_duration = float(face.shape[1])
 
-    #return sum([x * (num_dizitized**i) for i, x in enumerate(digitized)])
-    if mode == 'delta':
-        c_face=np.zeros(num_face)
-        c_face = np.mean(face,axis=1)-np.mean(face_before,axis=1)
-        reward = np.dot(c_face,c)
-
-    elif mode == 'heuristic':
-        h_face=np.zeros((num_face,time_window))
-        for face_type in range(num_face):
-            h_face[face_type,:]=h[face_type]*face[face_type,:]
-        reward = np.mean(h_face)
-
-    elif mode == 'predict':
-        e_face = face_predict[0,:type_face] - np.mean(face,axis=1)
-        print('seq.py/face_predict',face_predict[0,:type_face])
-        print('seq.py/face',np.mean(face,axis=1))
-        reward = math.fabs(1.0/np.mean(e_face))
-
     return reward
 
 if __name__ == "__main__" :
