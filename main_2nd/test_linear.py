cea20e080873342d12cfab7950e446046d903818
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-16 09:15:46 +0900

    checked

--- main_2nd/test_linear.py
+++ main_2nd/test_linear.py
@@ -13,6 +13,7 @@ def linear_state(single_state):
     #print('line/single_state',single_state.shape[1])
     a = np.zeros((single_state.shape[0],1))
     for i in range(single_state.shape[0]):
+        print('single_state',single_state[i,:])
         x = np.linspace(0,1,single_state[i,:].shape[0])
         #fitting
         b, c = np.polyfit(x, single_state[i,:], 1)
@@ -40,9 +41,13 @@ if __name__ == '__main__':
     type_face = 5
     type_ir = 5
 
-    state = np.zeros((type_face+type_ir,3))
-    state[:,0]=np.array([2,3,1,2,3,4,2,3,1,12])
-    state[:,1]=np.array([3,3,1,2,3,4,2,3,1,1])
-    state[:,2]=np.array([4,3,1,2,3,4,2,3,1,-10])
+    state1 = np.zeros((type_face+type_ir,1))
+    state2 = np.zeros((type_face+type_ir,1))
+    state3 = np.zeros((type_face+type_ir,1))
+    state1[:,0]=np.array([2,3,1,2,3,4,2,3,1,12])
+    state2[:,0]=np.array([3,3,1,2,3,4,2,3,1,1])
+    state3[:,0]=np.array([4,3,1,2,3,4,2,3,1,-10])
+    state=np.hstack((state1,state2,state3))
+    print('state',state)
     print(linear_state(state))
 
