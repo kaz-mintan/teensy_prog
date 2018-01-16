6b69fc0be7f3b8bcd744a2187385d3c5cbaff7b2
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-16 09:04:29 +0900

    modified to return array

--- main_2nd/test_linear.py
+++ main_2nd/test_linear.py
@@ -4,20 +4,22 @@ import math
 
 type_ir = 5
 
-def test_atan(a):
+def atan(a):
     val = float(a)
     return math.atan(val)*2/math.pi
 
 def linear_state(single_state):
-    print('line/single_state',single_state)
+    #print('line/single_state',single_state.shape[0])
+    #print('line/single_state',single_state.shape[1])
+    a = np.zeros((single_state.shape[0],1))
     for i in range(single_state.shape[1]):
         x = np.linspace(0,1,single_state[i,:].shape[0])
         #fitting
-        a, b = np.polyfit(x, single_state[i,:], 1)
+        b, c = np.polyfit(x, single_state[i,:], 1)
         #fitting line
-        a_float = float(a)
+        a[i,0] = atan(b)
 
-    return math.atan(a_float)*2/math.pi
+    return a
     #return a
 
 def check_thre(ir_sensor,thre):
@@ -31,6 +33,13 @@ def check_thre(ir_sensor,thre):
     return ret
 
 if __name__ == '__main__':
+    print('input a to test atan')
     a = raw_input()
-    print(test_atan(a))
+    print(atan(a))
+
+    type_face = 5
+    type_ir = 5
+
+    state = np.zeros((type_face+type_ir,5))
+    print(linear_state(state))
 
