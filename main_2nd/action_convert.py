b42df11908e573ff7624cd7815c3d74e7615b9e9
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-18 17:10:15 +0900

    rm output

--- main_2nd/action_convert.py
+++ main_2nd/action_convert.py
@@ -5,7 +5,6 @@ def convert_action(action):
     pwm_input = 30*action[0]+70
     keep = 1.0*action[1]+1.0
     delay = 1.5*action[2] + 1.0
-    print('action',pwm_input,keep,delay)
     return np.array([pwm_input,keep,delay])
 
 if __name__ == '__main__':
