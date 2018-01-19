b99195552f82bec88106c66297c1f88374efab85
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-20 05:44:07 +0900

    add devide

--- main_2nd/action_convert.py
+++ main_2nd/action_convert.py
@@ -1,11 +1,34 @@
 # coding:utf-8
 import numpy as np
 
+def devide_base(analog_base):
+    if analog_base>=0 and analog_base<0.2:
+        dib_base = 1
+    elif analog_base>=0.2 and analog_base<0.4:
+        dib_base = 2
+    elif analog_base>=0.4 and analog_base<0.6:
+        dib_base = 3
+    elif analog_base>=0.6 and analog_base<0.8:
+        dib_base = 4
+    elif analog_base>=0.8 and analog_base<1.0:
+        dib_base = 5
+    return dig_base
+
+def devide_2(analog_num):
+    if analog_num>=0 and analog_num<0.5:
+        dib_num = 1
+    elif analog_base>=0.5 and analog_base<1.0:
+        dib_num= 2
+    return dig_num
+
 def convert_action(action):
     pwm_input = 30*action[0]+70
-    keep = 1.0*action[1]+1.0
-    delay = 1.5*action[2] + 1.0
-    return np.array([pwm_input,keep,delay])
+    #keep = 1.0*action[1]+1.0
+    delay = 1.5*action[1] + 1.0
+    base = devide_base(action[2])
+    num = devide_2(action[3])
+    direction = devide_2(action[4])
+    return np.array([pwm_input,delay,base,num,direction])
 
 if __name__ == '__main__':
     type_action = 3
