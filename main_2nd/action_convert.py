70f8262f37a6c7abf9acba85a55d7aebb99efe8e
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-21 07:20:43 +0900

    module worked

--- main_2nd/action_convert.py
+++ main_2nd/action_convert.py
@@ -1,41 +1,45 @@
 # coding:utf-8
 import numpy as np
 
-def devide_base(analog_base):
-    if analog_base>=0 and analog_base<0.2:
-        dig_base = 0
-    elif analog_base>=0.2 and analog_base<0.4:
-        dig_base = 1
-    elif analog_base>=0.4 and analog_base<0.6:
-        dig_base = 2
-    elif analog_base>=0.6 and analog_base<0.8:
-        dig_base = 3
-    elif analog_base>=0.8 and analog_base<1.0:
-        dig_base = 4
-    return dig_base
+def select_array_num(symm,ir_no):
 
-def devide_2(analog_num):
-    if analog_num>=0 and analog_num<0.5:
-        dig_num = 0
-    elif analog_num>=0.5 and analog_num<1.0:
-        dig_num= 1
-    return dig_num
+    array=np.array([[2,2,2],
+        [6,8,5],
+        [11,14,12],
+        [17,20,18],
+        [22,22,22]])
 
-def convert_action(action):
-    pwm_input = 30*action[0]+70
+    arg_base= int(ir_no)
+    symmetry = int(symm)
+
+    return array[arg_base,symmetry]
+
+def devide_3(analog_num):
+    num = -1
+    array=np.linspace(0,1,4)
+    if analog_num == 1:
+        num = 2
+    else:
+        for i in range(4):
+            if analog_num < array[i] and analog_num>= array[i-1]:
+                num = i-1
+    return num
+
+def convert_action(action,ir_no):
+    pwm_input = 50*action[0]+30
     #keep = 1.0*action[1]+1.0
-    delay = 1.5*action[1] + 1.0
-    base = devide_base(action[2])
-    num = devide_2(action[3])
-    direction = devide_2(action[4])
-    return np.array([pwm_input,delay,base,num,direction])
+    delay = 2.5*action[1] + 0.5
+    symmetry = devide_3(action[2])
+    base = ir_no
+    array_num = select_array_num(symmetry,base)
+    return np.array([pwm_input,delay,array_num])
 
 if __name__ == '__main__':
     type_action = 3
     action = np.zeros((type_action,1))
     action[0,0]=1
-    action[1,0]=1
-    action[2,0]=1
-    print(action)
-    print(convert_action(action[:,0]))
+    action[1,0]=0.4
+    action[2,0]=0.1
+    ir_no = 2
+    print(convert_action(action[:,0],ir_no))
 
