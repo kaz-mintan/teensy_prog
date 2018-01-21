698f48a71d6ff912e591001cac3e97a937c8a7c5
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-22 05:36:12 +0900

    add date

--- main_2nd/experiment_1.sh
+++ main_2nd/experiment_1.sh
@@ -9,7 +9,23 @@ mode=$1
 sma_port=$2
 ir_port=$3
 number=$4
+today=$(date "+%Y%m%d")
 
-python motion_dqn_hard.py $1 /dev/ttyACM$2 /dev/ttyACM$3
+dir="/home/kumagai/data/waterloo/exp_"$today"_"$mode"_"$number
+mkdir -p $dir
+
+f1="test_state.csv"
+f2="test_state_mean.csv"
+f3="test_action_start.csv"
+f4="test_reward_face.csv"
+f5="reward_extracted.csv"
+f6="test_action_stop.csv"
+f7="test_reward.csv"
+f8="question_picture.csv"
+
+python motion_dqn_hard.py $mode /dev/ttyACM$sma_port /dev/ttyACM$ir_port
+
+mv $f1 $f2 $f3 $f4 $f5 $f6 $f7 $f8 $dir
 
 #python motion_dqn_hard.py heuristic /dev/ttyACM0 /dev/ttyACM1
+#csv files
