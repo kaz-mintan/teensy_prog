024dc67e5e3b921bffa33e5326c01df63043e4d2
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-21 12:27:10 +0900

    sma no changed and 5 sma moved in a same time

--- main_2nd/firmware/serial_5sma_order/serial_5sma_order.ino
+++ main_2nd/firmware/serial_5sma_order/serial_5sma_order.ino
@@ -1,13 +1,13 @@
 //#include <Servo.h>
 //Global variables
 
-#define TIME_ON   1.5   //In Seconds (Less than 2s)
+#define TIME_ON   0.1   //In Seconds (Less than 2s)
 #define TIME_OFF_CO  0.125    //In Seconds (At least 30s)
 
 #define SMA_PIN_1   9     //Pin # forj SMA PWM on P2
 #define SMA_PIN_2   10     //Pin # for SMA PWM on P2
-#define SMA_PIN_3   22     //Pin # for SMA PWM on P2
-#define SMA_PIN_4   23     //Pin # for SMA PWM on P2
+#define SMA_PIN_3   3     //Pin # for SMA PWM on P2
+#define SMA_PIN_4   32     //Pin # for SMA PWM on P2
 #define SMA_PIN_5   25     //Pin # for SMA PWM on P2
 
 
@@ -52,19 +52,13 @@ int serialNumVal() {
 
 int pin_no[5]={SMA_PIN_1,SMA_PIN_2,SMA_PIN_3,SMA_PIN_4,SMA_PIN_5};
 
-//int order_array[2][5]={{5,4,3,2,-1;},{1,2,-1,-1,-1,}}
-
-
-
 void act_sma(int pin_no, int deg){
-    Serial.println(pin_no);
     analogWrite(pin_no, map(deg, 0, 100, 0, 255));
-    //delay(TIME_ON*1000);
 }
 
 void stop_sma(int pin_no){
-    Serial.println(pin_no);
     analogWrite(pin_no, 0);
+    Serial.println(millis());
 }
 
 void send_all(int pwm_input,int keep,int delay_time, int order_array[2][5]){
@@ -74,57 +68,57 @@ void send_all(int pwm_input,int keep,int delay_time, int order_array[2][5]){
 	int move_array[5]={0,0,0,0,0};
 
 	int stop_array[5]={keep*100,
-		delay_time*100+keep*103,
-		delay_time*2*100+keep*106,
-		delay_time*3*110+keep*110,
-		delay_time*4*110+keep*110};
+		delay_time*100+keep*100,
+		delay_time*2*100+keep*100,
+		delay_time*3*100+keep*100,
+		delay_time*4*100+keep*100};
 
 	int start_array[5]={0,
 		delay_time*100,
 		delay_time*2*100,
-		delay_time*3*95,
-		delay_time*4*95};
-
+		delay_time*3*100,
+		delay_time*4*100};
+    Serial.println(delay_time);
 	//act_sma(SMA_PIN_1,pwm_input);
 	unsigned long now_time;
-  int dt;
+  unsigned long start_time_5[5];
+  int dt,dt_stop;
   int t_1,t_2;
 
   while(move_array[4]!=2){
- 
     now_time = millis();
 	  dt = now_time - start_time;
 
   	for(i = 0; i<5; i++){
-		t_1 = order_array[0][i];
-    t_2 = order_array[1][i];
-  		if(move_array[i]==0){
-          now_time = millis();
-          dt = now_time - start_time;
-  			if(dt>=start_array[i]){
-          if(t_1!=-1){
-  				  act_sma(pin_no[t_1],pwm_input);
-          }
-          if(t_2!=-1){
-            act_sma(pin_no[t_2],pwm_input);
-          }
-          move_array[i]=1;
-  			}
-  		}else if(move_array[i]==1){
-         now_time = millis();
-         dt = now_time - start_time;
-  			if(dt>=stop_array[i]){
-          if(t_1!=-1){
-  				  stop_sma(pin_no[t_1]);
-          }
-          if(t_2!=-1){
-            stop_sma(pin_no[t_2]);
-          }
-  				move_array[i]=2;
-  			}
+      if(move_array[i]==1){
+        dt_stop = now_time -start_time_5[i];
+      }
+  		t_1 = order_array[0][i];
+      t_2 = order_array[1][i];
+  		if(dt>start_array[i] && move_array[i]==0){
+                 Serial.println(i);
+                 Serial.println(millis());
+                 Serial.println("===");
+        if(t_1!=-1){
+				  act_sma(pin_no[t_1],pwm_input);
+        }
+        if(t_2!=-1){
+          act_sma(pin_no[t_2],pwm_input);
+        }
+        move_array[i]=1;
+        start_time_5[i]=millis();
+  		}
+  		if(dt_stop>3000){
+                 Serial.println("===");
+        if(t_1!=-1){
+				  stop_sma(pin_no[t_1]);
+        }
+        if(t_2!=-1){
+          stop_sma(pin_no[t_2]);
+        }
+				move_array[i]=2;
   		}
   	}
-   
   }
 }
 
@@ -157,6 +151,8 @@ int order_array[22][2][5] =  {{{4,3,2,1,0},{-1,-1,-1,-1,-1}}, //0 single close
         {{4,3,2,1,0},{-1,-1,-1,-1,-1}}};  //4 single apart
 
 // メインループ
+int hoge=0;
+  int hogehoge=0;
 void loop() {
   int pwm_input;
   float keep;
