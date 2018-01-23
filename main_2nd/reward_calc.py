2b43249c058f1bf1019bfd3a4066257a971b2c73
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-01-24 08:38:11 +0900

    dt added

--- main_2nd/reward_calc.py
+++ main_2nd/reward_calc.py
@@ -2,24 +2,31 @@
 # calculates reward based on recognized facial expression
 import numpy as np
 import math
+import time
 
 num_face = 5
 type_face = 5
 type_ir = 5
 
-def f(face):
+def f(face,t_array):
     face_next = face[:,1:] #for delta mode
-    do_sum = np.sum(face_next-face[:,:face.shape[1]-1],axis=1)
+    t_next = t_array[:,1:]
+    do = face_next-face[:,:face.shape[1]-1]
+    dt = t_next-t_array[:,:face.shape[1]-1]
+    dt_tile = np.tile(dt,(face.shape[0],1))
+    do_sum = np.sum(do/dt,axis=1)
 
     return do_sum
 
 def g(face):
-    amp_sum = np.sum(face - np.ones_like(face)*50,axis=1)
+    #amp_sum = np.sum(face - np.ones_like(face)*50,axis=1)
+    amp_sum = np.sum(face,axis=1)
     return amp_sum
 
-def reward_function(state, state_predict, state_before, mode):
+def reward_function(state, state_predict, state_before, mode, t_array):
     # extract face array (must be time sequence data)
     face = state[0:num_face,:] #in numpy, the 5 of the 0:5 is not included
+    t = t_array[0,:]
 
     # coefficient
     c_f = 10*np.array([0,70.0,70.0,-70.0,-70.0]) #for delta mode
@@ -30,7 +37,7 @@ def reward_function(state, state_predict, state_before, mode):
     #reward = np.sum(c_f*f(face)+c_g*g(face))
 
     if mode == 'delta':
-        reward = np.sum(c_f*f(face))
+        reward = np.sum(c_f*f(face,t))
 
     elif mode == 'heuristic':
         reward = np.sum(c_g*g(face))
@@ -47,8 +54,11 @@ if __name__ == "__main__" :
 
     state = np.ones((type_face+type_ir,4))
     state_ran = np.random.rand(type_face+type_ir,4)
+    dt_array=np.random.rand(1,4)
+    print('dt',dt_array)
+
     print('state_ran',state_ran)
     #state_ran = np.random.rand(5,5)
-    print('f(state)',f(np.array(state_ran[0:num_face,:])))
+    print('f(state)',f(np.array(state_ran[0:num_face,:]),dt_array))
     print('g(state)',g(np.array(state_ran[0:num_face,:])))
-    print(reward_function(state_ran,state,state,'heuristic'))
+    print(reward_function(state_ran,state,state,'heuristic',dt_array))
